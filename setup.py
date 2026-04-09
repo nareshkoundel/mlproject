9cdd816ba8f2f1feefe3cd8a5d0e72c0ac6ac12f
nareshkoundel <N.Koundel2336@student.leedsbeckett.ac.uk>
2026-04-09 21:47:13 +0100

    setup and reqirement

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements(file_path:str)->list[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .') 
+            
+    return requirements
+
+
+setup(
+name='MLPROJECTS',
+version='0.0.1',
+author='Naresh',
+author_email='naresh03koundel11@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
